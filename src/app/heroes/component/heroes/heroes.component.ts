83e6bf159fc24998802e107e1cd535af75559b7d
ayanny <ayanny_maria@hotmail.com>
2024-01-28 00:05:08 -0300

    feat: criacao componente heroes

---
+++ src/app/heroes/component/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../model/hero.model';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 0,
+    name: ''
+  }
+
+  ngOnInit(): void {}
+
+}
